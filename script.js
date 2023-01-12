458463d808e64cd894add8c7e7d4d65ff1392c86
AndreaDeGiorgi00 <andrea.adedegiorgi@gmail.com>
2023-01-12 13:01:40 +0100

    background

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("vue Ok " , Vue)
+
+
+const app = Vue.createApp({
+    data(){
+        return{
+            allInfos : data ,
+            
+        }},
+})
+
+
+app.mount(".target")
+console.log(data)
+
+
